600c5fca33887b717ecdd7efc6c2b720b735948a
Valentin Georgiev <valentin.georgiev@farmbio.uu.se>
2017-12-13 13:00:07 +0100

    done with parsing of a single txt file
    and making records in DB

--- parseWinLos.py
+++ parseWinLos.py
@@ -1,6 +1,9 @@
 import re
+import os
+import sqlite3 as lite
+import codecs
 
-hand = open('2017-12-12-vinnare-forlorareNew.txt')
+hand = codecs.open('2017-12-12-vinnare-forlorareNew.txt', encoding='utf-8')
 numbers = list()
 
 entries = []
@@ -25,15 +28,24 @@ for line in hand:
 
       entries.append(line.strip())
 
+def connectDB():
 
+      db_file = dir_path + "/win_los.db"
+      return  lite.connect(db_file)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+db = connectDB()
+cur = db.cursor()
 for stock in stocks:
 
 	stock[2] = stock[2].replace("[u'", "")
 	stock[2] = stock[2].replace("']", "")
 	stock[1] = stock[1].replace("/aktier/om-aktien.html/", "")
 	print stock
+	query = u"INSERT INTO win_los (sname, slink, pchange, date) VALUES (\'%s\', \'%s\', \'%s\', \'%s\');" % (stock[0], stock[1], stock[2], stock[3])
+	cur.execute(query)
 
-
+db.commit()
 
 
 
