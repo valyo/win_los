cf1f7e6172ccd1cba496f21612ca71a1171618f1
Valentin Georgiev <valentin.georgiev@farmbio.uu.se>
2017-12-14 22:07:32 +0100

    implemented creating of a list instead of
    printing

--- getWinLos.01.py
+++ getWinLos.01.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 import requests
 import sqlite3 as lite
 from operator import itemgetter
+from datetime import datetime, timedelta
 # from HTMLParser import HTMLParser
 # from IPython.core.debugger import Tracer
 
@@ -16,27 +17,55 @@ from operator import itemgetter
 #     output_file = sys.argv[1]
 # except:
 #     sys.exit("no output file specified!")
+currentDate = datetime.today().strftime('%Y-%m-%d')
+
+entries = []
+
 def get_tables_data(tables):
     for tr in tables[0].find_all("tr"):
-        print tr.find_all("a", class_="link")[0].string.encode('utf-8')
-        print tr.find_all("a", class_="link")[0].get('href')
-        print tr.find("td", class_="positive changePercent").string.encode('utf-8')
+    	win = []
+        # print tr.find_all("a", class_="link")[0].string.encode('utf-8')
+        sname = tr.find_all("a", class_="link")[0].string.encode('utf-8')
+        win.append(sname.strip())
+        # print tr.find_all("a", class_="link")[0].get('href')
+        slink =  tr.find_all("a", class_="link")[0].get('href').replace("/aktier/om-aktien.html/", "")
+        win.append(slink)
+        # print tr.find("td", class_="positive changePercent").string.encode('utf-8')
+        pchange = tr.find("td", class_="positive changePercent").string.encode('utf-8').replace(",", ".")
+        win.append(pchange)
+        plast = tr.find("td", class_="lastPrice last").string.encode('utf-8').replace(",", ".")
+        win.append(plast)
+        win.append(currentDate)
+        win.append("W")
+        entries.append(win)
 
     for tr in tables[1].find_all("tr"):
-        print tr.find_all("a", class_="link")[0].string.encode('utf-8')
-        print tr.find_all("a", class_="link")[0].get('href')
-        print tr.find("td", class_="negative changePercent").string.encode('utf-8')
+    	los = []
+        sname = tr.find_all("a", class_="link")[0].string.encode('utf-8')
+        los.append(sname.strip())
+        # print tr.find_all("a", class_="link")[0].get('href')
+        slink = tr.find_all("a", class_="link")[0].get('href').replace("/aktier/om-aktien.html/", "")
+        los.append(slink)
+        # print tr.find("td", class_="negative changePercent").string.encode('utf-8')
+        pchange = tr.find("td", class_="negative changePercent").string.encode('utf-8').replace(",", ".")
+        los.append(pchange)
+        plast = tr.find("td", class_="lastPrice last").string.encode('utf-8').replace(",", ".")
+        los.append(plast)
+        los.append(currentDate)
+        los.append("L")
+        entries.append(los)
+    return entries
 
 
 # url = "file:///Users/valentingeorgiev/dev/winners_loosers/2016-12-14-vinnare-forlorare.html"
 # url = "https://www.avanza.se/marknadsoversikt.html"
-url_all = "https://www.avanza.se/aktier/vinnare-forlorare.html"
-r = requests.get(url_all)
-data = r.text
-soup = BeautifulSoup(data, "lxml")
-all_tables = soup.find_all("tbody")
-print "==All stocks=="
-get_tables_data(all_tables)
+# url_all = "https://www.avanza.se/aktier/vinnare-forlorare.html"
+# r = requests.get(url_all)
+# data = r.text
+# soup = BeautifulSoup(data, "lxml")
+# all_tables = soup.find_all("tbody")
+# print "==All stocks=="
+# get_tables_data(all_tables)
 
 # print len(soup.find_all("table"))
 # print len(all_tables)
@@ -47,8 +76,10 @@ r = requests.get(url_stchlm)
 data = r.text
 soup = BeautifulSoup(data, "lxml")
 all_tables = soup.find_all("tbody")
-print "\n ==XSTO stocks=="
-get_tables_data(all_tables)
+# print "\n ==XSTO stocks=="
+res = get_tables_data(all_tables)
+for r in res:
+	print r
 # print all_tables[1].find_all("tr")[0].find_all("a", class_="link")[0].get('href')
 
 # for tr in all_tables[0].find_all("tr"):
