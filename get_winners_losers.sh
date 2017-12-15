c8e66dff51ada2f5701cc53c094046e777115d22
Valentin Georgiev <zaetnick@gmail.com>
2017-12-15 22:57:02 +0100

    fix typo in dir name

--- get_winners_losers.sh
+++ get_winners_losers.sh
@@ -4,12 +4,12 @@ PYTHON=$(which python)
 DATE=$(date +"%Y-%m-%d")
 
 #check if an output file from taday exists
-if [ ! -s $HOME/winners_loosers/$DATE-vinnare-forlorareNew.txt ]
+if [ ! -s $HOME/winners_losers/$DATE-vinnare-forlorareNew.txt ]
 then
-  $PYTHON $HOME/winners_loosers/getWinLos.py > $HOME/winners_loosers/$DATE-vinnare-forlorareNew.txt
-  echo "collected $DATE" >> $HOME/winners_loosers/log.txt
+  $PYTHON $HOME/winners_losers/getWinLos.py > $HOME/winners_losers/$DATE-vinnare-forlorareNew.txt
+  echo "collected $DATE" >> $HOME/winners_losers/log.txt
 else
-  echo "nothing to do $(date)" >> $HOME/winners_loosers/log.txt
+  echo "nothing to do $(date)" >> $HOME/winners_losers/log.txt
 fi
 
-$PYTHON $HOME/winners_loosers/getWinLos.01.py
+$PYTHON $HOME/winners_losers/getWinLos.01.py
